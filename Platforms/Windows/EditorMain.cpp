9f4a209503cabd4c43bead4046b61302b79797cb
Zenkovich <andrey.zenkovich@playrix.com>
2021-03-11 21:44:51 +0300

    Update o2

--- Platforms/Windows/EditorMain.cpp
+++ Platforms/Windows/EditorMain.cpp
@@ -14,57 +14,9 @@ DECLARE_SINGLETON(Editor::WindowsManager);
 DECLARE_SINGLETON(Editor::EditorConfig);
 DECLARE_SINGLETON(Editor::ToolsPanel);
 
-
-template<typename base>
-struct A: public base
-{
-	A& DoSmth() { base::DoSmth(); printf("A"); return *this; }
-
-	template<template<typename> class mixin>
-	mixin<A<base>> Mix()
-	{
-		return MixImpl<mixin, A<base>>();
-	}
-};
-
-template<typename base>
-struct B: public base
-{
-	B& DoSmth() { base::DoSmth(); printf("B"); return *this; }
-
-	template<template<typename> class mixin>
-	mixin<B<base>> Mix()
-	{
-		return MixImpl<mixin, B<base>>();
-	}
-};
-
-struct Basic
-{
-	Basic& DoSmth() { printf("Basic"); return *this; }
-
-	template<template<typename> class mixin>
-	mixin<Basic> Mix()
-	{
-		return MixImpl<mixin, Basic>();
-	}
-
-	template<template<typename> class mixin, class basemix>
-	mixin<basemix> MixImpl()
-	{
-		return mixin<basemix>();
-	}
-};
-
 int main()
 {
 	INITIALIZE_O2;
-
-	Basic x;
-	x.Mix<A>().Mix<B>().DoSmth(); 
-	printf("\n");
-	x.Mix<B>().Mix<A>().DoSmth();
-	printf("\n");
 
 	Editor::EditorApplication* app = mnew Editor::EditorApplication();
 	app->Initialize();
