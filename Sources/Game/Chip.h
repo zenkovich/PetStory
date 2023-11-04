6955a85bd36e66eb457c8a159ff5c8e6060a2c05
Zenkovich Andrey <andrei.zenkovich@playrix.com>
2023-11-04 21:12:59 +0200

    Update o2

--- Sources/Game/Chip.h
+++ Sources/Game/Chip.h
@@ -24,3 +24,27 @@ private:
 private:
 	 void OnCursorReleased(const Input::Cursor& cursor) override;
 };
+// --- META ---
+
+CLASS_BASES_META(Chip)
+{
+	BASE_CLASS(Component);
+	BASE_CLASS(CursorAreaEventsListener);
+}
+END_META;
+CLASS_FIELDS_META(Chip)
+{
+	FIELD().PRIVATE().EDITOR_PROPERTY_ATTRIBUTE().SERIALIZABLE_ATTRIBUTE().NAME(mType);
+	FIELD().PRIVATE().EDITOR_PROPERTY_ATTRIBUTE().SERIALIZABLE_ATTRIBUTE().DEFAULT_VALUE(230.0f).NAME(mGroupingRadius);
+	FIELD().PRIVATE().NAME(mImage);
+}
+END_META;
+CLASS_METHODS_META(Chip)
+{
+
+	FUNCTION().PUBLIC().SIGNATURE(bool, IsUnderPoint, const Vec2F&);
+	FUNCTION().PUBLIC().SIGNATURE(void, OnStart);
+	FUNCTION().PRIVATE().SIGNATURE(void, OnCursorReleased, const Input::Cursor&);
+}
+END_META;
+// --- END META ---
